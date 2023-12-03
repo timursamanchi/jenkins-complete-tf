eee5b55d24dd5f7c777dea0cb67e5a9f09146e91
timursamanchi <timur.samanchi@gmail.com>
2023-12-03 17:15:21 +0000

    initial push

--- jenkins-install-script.sh
+++ jenkins-install-script.sh
@@ -13,8 +13,8 @@ echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
     /etc/apt/sources.list.d/jenkins.list > /dev/null
 
 sudo apt-get update
-sudo apt-get install fontconfig openjdk-17-jre
-sudo apt-get install jenkins
+sudo apt-get -y install fontconfig openjdk-17-jre
+sudo apt-get -y install jenkins
 
 sudo apt-get -y install build-essential 
 sudo apt-get -y install python3
@@ -30,6 +30,13 @@ sudo apt-get -y install \
                 libreadline-dev \
                 libsqlite3-dev \
                 zlib1g-dev
+
+sudo fallocate -l 1G /var/swapfile
+sudo chmod 600 /var/swapfile
+sudo mkswap /var/swapfile
+sudo swapon /var/swapfile
+echo '/var/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+sudo sysctl -p
 
 sudo systemctl start jenkins
 sudo systemctl enable jenkins

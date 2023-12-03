a071eb438ffcb4c8b7a5a5d9ead490bffcef24ae
timursamanchi <timur.samanchi@gmail.com>
2023-12-03 16:27:16 +0000

    initial push

--- jenkins-install-script.sh
+++ jenkins-install-script.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
+echo 'Hello, World!' > /tmp/hello.txt
+
 sudo apt-get update
 sudo apt-get -y upgrade
+
+sudo apt-get -y install build-essential
+sudo apt-get -y install nginx 
+sudo apt-get -y install git 
+sudo apt-get -y install python3
+sudo apt-get -y install python3-pip
+
+sudo systemctl restart nginx
